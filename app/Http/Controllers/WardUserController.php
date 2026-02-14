073becb536c8ed16bae39fcaef9be930a33f50da
Cristiano Carlos <cristianoc@gmail.com>
2026-02-14 16:52:18 -0300

    .

---
+++ app/Http/Controllers/WardUserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class WardUserController extends Controller
+{
+    public function create(Request $request): JsonResponse {
+        return response()->json([]);
+    }
+
+    public function delete(Request $request): JsonResponse {
+        return response()->json([]);
+    }
+
+    public function index(Request $request): JsonResponse {
+        return response()->json([]);
+    }
+
+    public function update(Request $request): JsonResponse {
+        return response()->json([]);
+    }
+
+    public function view(Request $request): JsonResponse {
+        return response()->json([]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,19 @@
 <?php
 
 use App\Http\Controllers\WardAuthController;
+use App\Http\Controllers\WardUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('ward')->group(function () {
     Route::post('/login', [WardAuthController::class, 'login'])->middleware('throttle:6,1');
     Route::post('/refresh', [WardAuthController::class, 'refresh']);
-    Route::post('/deactivate', [WardAuthController::class, 'deactivate']);
-    Route::post('/reactivate', [WardAuthController::class, 'reactivate']);
     Route::middleware('auth:ward')->group(function () {
         Route::post('/logout', [WardAuthController::class, 'logout']);
+        //
+        Route::post('/user/create', [WardUserController::class, 'create']);
+        Route::delete('/user/delete/{model}', [WardUserController::class, 'delete']);
+        Route::get('/user/index', [WardUserController::class, 'index']);
+        Route::put('/user/update/{model}', [WardUserController::class, 'update']);
+        Route::get('/user/view/{model}', [WardUserController::class, 'view']);
     });
 });
