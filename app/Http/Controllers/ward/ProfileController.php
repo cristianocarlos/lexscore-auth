013df7cc4ea108189fcc28ddcbdd21e71dd52199
Cristiano Silva <cristianoc@gmail.com>
2026-03-25 17:25:56 -0300

    Update ProfileController.php

--- app/Http/Controllers/ward/ProfileController.php
+++ app/Http/Controllers/ward/ProfileController.php
@@ -6,8 +6,10 @@ use App\Custom\JwtHelper;
 use App\Enums\YiiEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ward\UserRequest;
+use App\Http\Resources\JsonFeedbackResource;
 use App\Http\Resources\ward\user\CrudUserSaveResource;
 use App\Http\Resources\ward\user\CrudUserViewResource;
+use App\Models\ward\AuthUser as WardAuthUser;
 use App\Models\ward\CrudUser as WardCrudUser;
 use App\Models\ward\UserToken;
 use App\Services\EmailService;
@@ -16,6 +18,21 @@ use Illuminate\Http\JsonResponse;
 class ProfileController extends Controller
 {
     public function passwordUpdate(): JsonResponse {
+        request()->validate([
+            'current_password' => 'required',
+            'new_password' => 'required|confirmed',
+        ]);
+        $authUser = JwtHelper::getAuthUser();
+        $model = WardAuthUser::find($authUser->id);
+        if (!$model->validatePassword(request()->input('current_password'))) {
+            return response()->json(
+                new JsonFeedbackResource(errors: ['current_password' => ['Senha incorreta']]),
+                JsonResponse::HTTP_UNPROCESSABLE_ENTITY,
+            );
+        }
+        $model->user_pass = request()->input('new_password');
+        $model->save();
+        return response()->json(new JsonFeedbackResource('save'));
     }
 
     public function personalInfoUpdate(UserRequest $request): JsonResponse {
@@ -43,7 +60,7 @@ class ProfileController extends Controller
             try {
                 $userTokenModel = UserToken::tokenSave(userId: $authUser->id, email: request('email'), typeId: YiiEnum::USER_TOKEN_EMAIL_CHANGE->value);
                 $emailService->userEmailChangeSend($userTokenModel->ustk_toke, $userTokenModel->ustk_mail, request('host'));
-            } catch (\Exception $e) {
+            } catch (\Exception $_e) {
                 // Se der ruim aqui tanto faz, o usuário pode clicar no resend
             }
         }
